ae520544a00fa666c96476b4576a175a78b8833f
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-07 15:35:34 +0300

    Add Date property to INotification interface
    Make the properties explicitly public and add the Date property to the INotification interface to facilitate timestamped notifications. This ensures better tracking and sorting of notifications by date and time.

--- src/Gml.Core.Interfaces/Notifications/INotification.cs
+++ src/Gml.Core.Interfaces/Notifications/INotification.cs
@@ -1,10 +1,12 @@
+using System;
 using GmlCore.Interfaces.Enums;
 
 namespace GmlCore.Interfaces.Notifications;
 
 public interface INotification
 {
-    string Message { get; set; }
-    string Details { get; set; }
+    public string Message { get; set; }
+    public string Details { get; set; }
     public NotificationType Type { get; set; }
+    public DateTimeOffset Date { get; set; }
 }
