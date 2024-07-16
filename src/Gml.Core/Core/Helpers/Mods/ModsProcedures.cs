f333fdb70faeba0d5ed503730ff1376f36989e9f
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-16 15:37:57 +0300

    Add Mod Procedures and integrate Modrinth API
    Introduced a new interface IModsProcedures and its implementation, ModsProcedures, to handle mod retrieval operations. Integrated the Modrinth API as a submodule and updated relevant project files and solution structure. Added IMod interface for mod data representation.

---
+++ src/Gml.Core/Core/Helpers/Mods/ModsProcedures.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GmlCore.Interfaces.Launcher;
+using GmlCore.Interfaces.Mods;
+using GmlCore.Interfaces.Procedures;
+using Modrinth.Api;
+using Modrinth.Api.Core.Projects;
+// using Modrinth.Api.Domains.Models.Dto;
+
+namespace Gml.Core.Helpers.Mods;
+
+public class ModsProcedures : IModsProcedures
+{
+    private readonly ModrinthApi _modrinthApi = new();
+
+    public async Task<IEnumerable<IMod>> GetModsAsync(IGameProfile profile)
+    {
+        // Task<SearchProjectResultDto>? mods = _modrinthApi.ModsProject.FindAsync();
+
+
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<IMod>> GetModsAsync(IGameProfile profile, string name)
+    {
+        throw new NotImplementedException();
+    }
+}
