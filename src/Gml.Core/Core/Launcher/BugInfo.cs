c3aed3471bdf3f71d1cef25ce29838e04615667b
akemiko <tcop29711@gmail.com>
2024-08-08 12:38:53 +0300

    Add Bug and BugInfo classes to handle bug details
    This commit introduces additional properties to the BugInfo class, including a list of Bug objects. Each Bug object captures details such as PcName, PcUsername, Username, Exception, IpAddress, OsType, and OsIdentifier to better track and manage bug information.

--- src/Gml.Core/Core/Launcher/BugInfo.cs
+++ src/Gml.Core/Core/Launcher/BugInfo.cs
@@ -1,8 +1,20 @@
+using System.Collections.Generic;
 using GmlCore.Interfaces.Launcher;
 
 namespace Gml.Core.Launcher;
 
 public class BugInfo : IBugInfo
 {
+    public List<Bug> Bugs { get; set; }
+}
 
+public class Bug
+{
+    public string? PcName { get; set; }
+    public string? PcUsername { get; set; }
+    public string? Username { get; set; }
+    public string? Exception { get; set; }
+    public string? IpAddress { get; set; }
+    public string? OsType { get; set; }
+    public string? OsIdentifier { get; set; }
 }
