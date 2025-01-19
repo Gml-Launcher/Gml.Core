f393c53e0b067f96f82fe83b8c7ef2b8ecc1ccb0
PyWhy <myxi2002@gmail.com>
2025-01-19 18:41:53 +0500

    Добавление апи CurseForge

--- src/Gml.Core/Core/Helpers/Mods/ModsProcedures.cs
+++ src/Gml.Core/Core/Helpers/Mods/ModsProcedures.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using CurseForge.APIClient;
 using Gml.Core.Constants;
 using Gml.Core.Extensions;
 using Gml.Core.Services.Storage;
@@ -21,6 +22,8 @@ namespace Gml.Core.Helpers.Mods;
 public class ModsProcedures(IGmlSettings settings, IStorageService storage, IBugTrackerProcedures bugTracker) : IModsProcedures
 {
     private readonly ModrinthApi _modrinthApi = new(Environment.CurrentDirectory, settings.HttpClient);
+    private readonly ApiClient _curseForgeApi = new("$2a$10$sKe17GC2.HMeU7E7OC3osOLcTCxhAqiR8Dups8GRE4uSRevbGQrWK"); // TODO: нужно сделать реальзаци сохранение в панель ключа апи для курса фордж
+    private readonly int _curseForgeGameId = 432;
     private ConcurrentDictionary<string, ModInfo> _modsInfo = [];
     public ICollection<IModInfo> ModsDetails => _modsInfo.Values.OfType<IModInfo>().ToArray();
 
@@ -29,6 +32,7 @@ public class ModsProcedures(IGmlSettings settings, IStorageService storage, IBug
         return profile.GetModsAsync();
     }
 
+    // TODO: выбор откуда брать данные CurseForge или Modrinth
     public Task<IEnumerable<IMod>> GetModsAsync(IGameProfile profile, string name)
     {
         throw new NotImplementedException();
@@ -36,24 +40,51 @@ public class ModsProcedures(IGmlSettings settings, IStorageService storage, IBug
 
     public async Task<IExternalMod?> GetInfo(string identify)
     {
+        return await GetInfoModByCurseForge(identify);
+    }
+
+    private async Task<IExternalMod?> GetInfoModByModrinth(string identify)
+    {
         var mod = await _modrinthApi.Mods.FindAsync<ModProject>(identify, CancellationToken.None)
             .ConfigureAwait(false);
 
         return mod is null
             ?  null
             : new ModrinthMod
+            {
+                Id = mod.Id,
+                Name = mod.Title,
+                Description = mod.Description,
+                FollowsCount = mod.Followers,
+                DownloadCount = mod.Downloads,
+                IconUrl = mod.IconUrl
+            };
+    }
+
+    private async Task<IExternalMod?> GetInfoModByCurseForge(string identify)
+    {
+        var mod = await _curseForgeApi.GetModAsync(Int32.Parse(identify))
+            .ConfigureAwait(false);
+
+        return mod is null ? null : new CurseForgeMod
         {
-            Id = mod.Id,
-            Name = mod.Title,
-            Description = mod.Description,
-            FollowsCount = mod.Followers,
-            DownloadCount = mod.Downloads,
-            IconUrl = mod.IconUrl
+            Id = mod.Data.Id.ToString(),
+            Name = mod.Data.Name,
+            Description = mod.Data.Summary,
+            FollowsCount = Convert.ToInt32(mod.Data.Rating),
+            DownloadCount = Convert.ToInt32(mod.Data.DownloadCount),
+            IconUrl = mod.Data.Logo.Url
         };
     }
 
+
     public async Task<IReadOnlyCollection<IModVersion>> GetVersions(IExternalMod modInfo, GameLoader profileLoader,
         string gameVersion)
+    {
+        return await GetCurseForgeModVersions(modInfo, profileLoader, gameVersion);
+    }
+
+    private async Task<IReadOnlyCollection<IModVersion>> GetModrinthModVersions(IExternalMod modInfo, GameLoader profileLoader, string gameVersion)
     {
         var versions = await _modrinthApi.Versions
             .GetVersionsByModId(modInfo.Id, profileLoader.ToModrinthString(), gameVersion, CancellationToken.None)
@@ -71,11 +102,39 @@ public class ModsProcedures(IGmlSettings settings, IStorageService storage, IBug
         }).ToArray();
     }
 
+    private async Task<IReadOnlyCollection<IModVersion>> GetCurseForgeModVersions(IExternalMod modInfo,
+        GameLoader profileLoader, string gameVersion)
+    {
+        var versions = await _curseForgeApi.GetModFilesAsync(
+                modId: Int32.Parse(modInfo.Id),
+                gameVersion: gameVersion,
+                modLoaderType: profileLoader.ToCurseForge()
+                )
+            .ConfigureAwait(false);
+        return versions.Data.Select(version => new CurseForgeModVersion
+        {
+            Id = version.Id.ToString(),
+            Name = version.FileName,
+            VersionName = version.FileName,
+            DatePublished = version.FileDate,
+            Downloads = Convert.ToInt32(version.DownloadCount),
+            Files = new List<string> { version.DownloadUrl }
+        }).ToArray();
+    }
+
+    // TODO: нужно сделать реализацию поиска модов по выбору где искать CurseForge или Modrinth
     public async Task<IEnumerable<IMod>> FindModsAsync(
         GameLoader profileLoader,
         string gameVersion,
         string modName,
         short take,
+        short offset)
+    {
+        return await FindByCurseForgeMods(profileLoader, gameVersion, modName, take, offset);
+    }
+
+    // Работа с Modrinth
+    private async Task<IEnumerable<IMod>> FindByModrinthMods(GameLoader profileLoader, string gameVersion, string modName, short take,
         short offset)
     {
         var searchFilter = new ProjectModFilter
@@ -103,21 +162,44 @@ public class ModsProcedures(IGmlSettings settings, IStorageService storage, IBug
         });
     }
 
+    // Работа с CurseForge
+    private async Task<IEnumerable<IMod>> FindByCurseForgeMods(GameLoader profileLoader, string gameVersion, string modName, short take,
+        short offset)
+    {
+        var mods = await _curseForgeApi.SearchModsAsync(
+                gameId: _curseForgeGameId,
+                gameVersion: gameVersion,
+                modLoaderType: profileLoader.ToCurseForge(),
+                searchFilter: modName
+                )
+            .ConfigureAwait(false);
+        return mods.Data.Select(mod => new CurseForgeMod
+        {
+            Id = mod.Id.ToString(),
+            Name = mod.Name,
+            Description = mod.Summary,
+            FollowsCount = Convert.ToInt32(mod.Rating),
+            DownloadCount = Convert.ToInt32(mod.DownloadCount),
+            IconUrl = mod.Logo.Url,
+        });
+    }
+
+    // TODO: нужно версию так же выбирать откуда брать данные CurseForge или Modrinth
     public Task SetModDetails(string modName, string title, string description)
     {
         _modsInfo.AddOrUpdate(modName,
-        _ => new ModInfo
-        {
-            Key = modName,
-            Title = title,
-            Description = description,
-        },
-        (_, existing) =>
-        {
-            existing.Title = title;
-            existing.Description = description;
-            return existing;
-        });
+            _ => new ModInfo
+            {
+                Key = modName,
+                Title = title,
+                Description = description,
+            },
+            (_, existing) =>
+            {
+                existing.Title = title;
+                existing.Description = description;
+                return existing;
+            });
 
         return storage.SetAsync(StorageConstants.ModsInfo, _modsInfo);
     }
---
+++ src/Gml.Core/Models/Mods/CurseForgeMod.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using GmlCore.Interfaces.Mods;
+
+namespace Gml.Models.Mods;
+
+public class CurseForgeMod : IExternalMod
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+    public string IconUrl { get; set; }
+    public int DownloadCount { get; set; }
+    public int FollowsCount { get; set; }
+    public IReadOnlyCollection<string> Files { get; set; }
+    public IReadOnlyCollection<IMod> Dependencies { get; set; }
+    public ModType Type { get; }
+    public Stream Icon { get; set; }
+}
