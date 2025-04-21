1681933075af0902d75d9854cff6d12d7d607b7e
Terentev A. A. <terentev.a.a@recloud.tech>
2025-04-21 22:32:44 +0300

    Refactor ReplaceParameterVisitor to use record constructor.
    Simplified the ReplaceParameterVisitor class by replacing private fields and explicit constructor with a record-style constructor. This improves readability and reduces boilerplate code.

--- src/Gml.Core/Core/Services/Storage/ReplaceParameterVisitor.cs
+++ src/Gml.Core/Core/Services/Storage/ReplaceParameterVisitor.cs
@@ -2,19 +2,11 @@ using System.Linq.Expressions;
 
 namespace Gml.Core.Services.Storage;
 
-public class ReplaceParameterVisitor : ExpressionVisitor
+public class ReplaceParameterVisitor(ParameterExpression oldParameter, ParameterExpression newParameter)
+    : ExpressionVisitor
 {
-    private readonly ParameterExpression _oldParameter;
-    private readonly ParameterExpression _newParameter;
-
-    public ReplaceParameterVisitor(ParameterExpression oldParameter, ParameterExpression newParameter)
-    {
-        _oldParameter = oldParameter;
-        _newParameter = newParameter;
-    }
-
     protected override Expression VisitParameter(ParameterExpression node)
     {
-        return node == _oldParameter ? _newParameter : base.VisitParameter(node);
+        return node == oldParameter ? newParameter : base.VisitParameter(node);
     }
 }
