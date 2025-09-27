93ad12ed08574437da3f32b6e1c718dc5d96d44f
Terentev A. A. <terentev.a.a@recloud.tech>
2025-09-27 13:22:34 +0300

    Enhance `IAccessTokenService`: add `tokenDuration` parameter to token generation methods for configurable token lifetimes.

--- src/Gml.Core.Interfaces/Auth/IAccessTokenService.cs
+++ src/Gml.Core.Interfaces/Auth/IAccessTokenService.cs
@@ -5,13 +5,15 @@ namespace GmlCore.Interfaces.Auth;
 public interface IAccessTokenService
 {
     string GenerateAccessToken(int userId, string? role = null);
-    string GenerateAccessToken(string subject, string? role = null);
+    string GenerateAccessToken(string subject, int? tokenDuration, string? role = null);
 
     string GenerateAccessToken(int userId, string userLogin, string userEmail, IEnumerable<string> roles,
-        IEnumerable<string> permissions);
+        IEnumerable<string> permissions, int? tokenDuration = null);
 
-    string GenerateAccessToken(string subject, IEnumerable<string> roles, IEnumerable<string> permissions);
-    string GenerateAccessToken(string subject, string userLogin, string userEmail, IEnumerable<string> roles, IEnumerable<string> permissions);
+    string GenerateAccessToken(string subject, IEnumerable<string> roles, IEnumerable<string> permissions,
+        int? tokenDuration = null);
+    string GenerateAccessToken(string subject, string userLogin, string userEmail, IEnumerable<string> roles,
+        IEnumerable<string> permissions, int? tokenDuration = null);
     bool ValidateToken(string token);
     string GenerateRefreshToken();
     string HashRefreshToken(string refreshToken);
