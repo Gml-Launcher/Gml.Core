7b4d55a5b0d3f74468f64d3f09bcbf55ccbc373f
gamervii <terentev.a.a@recloud.tech>
2025-11-13 09:19:58 +0300

    Update macOS mirror URLs in `MirrorsHelper` for .NET SDK 8.0.302

--- src/Gml.Core/Core/Helpers/Mirrors/MirrorsHelper.cs
+++ src/Gml.Core/Core/Helpers/Mirrors/MirrorsHelper.cs
@@ -57,11 +57,11 @@ public class MirrorsHelper
         },
         {
             "osx", [
-                // "https://mirror.recloud.tech/dotnet-sdk-8.0.302-win-x64.zip",
-                // "https://mirror.recloud.host/dotnet-sdk-8.0.302-win-x64.zip",
-                // "https://mr-1.recloud.tech/dotnet-sdk-8.0.302-win-x64.zip",
-                // "https://mr-2.recloud.tech/dotnet-sdk-8.0.302-win-x64.zip",
-                // "https://mr-3.recloud.tech/dotnet-sdk-8.0.302-win-x64.zip",
+                "https://mirror.recloud.tech/dotnet-sdk-8.0.302-macos-aarch64.zip",
+                "https://mirror.recloud.host/dotnet-sdk-8.0.302-macos-aarch64.zip",
+                "https://mr-1.recloud.tech/dotnet-sdk-8.0.302-macos-aarch64.zip",
+                "https://mr-2.recloud.tech/dotnet-sdk-8.0.302-macos-aarch64.zip",
+                "https://mr-3.recloud.tech/dotnet-sdk-8.0.302-macos-aarch64.zip",
             ]
         },
     };
